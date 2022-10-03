15c429b42b231565d79276819c24be3d4354d560
Mirish013 <109863440+Mirish013@users.noreply.github.com>
2022-10-03 18:38:11 +0400

    Update Program.cs

--- Calculat-e/Program.cs
+++ Calculat-e/Program.cs
@@ -4,6 +4,15 @@
   {
     public static void Main(string[] args)
     {
+      Console.WriteLine(NeticeGoster(10,"/",10));
+      Console.ReadLine();
     }
+    static double NeticeGoster( double x, string emel, double y)
+        {
+            calculate hes = new calculate();
+            hes.Setstr(emel,x,y);
+            var qiy = hes.Getstr();
+            return qiy;
+        }
   }
 }
