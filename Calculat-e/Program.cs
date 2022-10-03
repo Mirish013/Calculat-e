c118c14f3c8caa8981a7eed38ac4adb1ff420860
Mirish013 <109863440+Mirish013@users.noreply.github.com>
2022-10-03 18:16:57 +0400

    Initial commit

---
+++ Calculat-e/Program.cs
@@ -0,0 +1,9 @@
+﻿namespace Calculat_e
+{
+  internal class Program
+  {
+    public static void Main(string[] args)
+    {
+    }
+  }
+}
---
+++ Calculat-e/calculate.cs
@@ -0,0 +1,31 @@
+﻿namespace Calculating
+{
+    public class calculate
+    {
+        private double qiy;
+
+        public double Getstr()
+        {
+            return qiy;
+        }
+
+        public void Setstr(string str, double x1, double y1)
+        {
+            switch (str)
+            {
+                case "/":
+                    qiy = x1 / y1;
+                    break;
+                case "*":
+                    qiy = x1 * y1;
+                    break;
+                case "-":
+                    qiy = x1 - y1;
+                    break;
+                case "+":
+                    qiy = x1 + y1;
+                    break;
+            }
+        }
+    }
+}
